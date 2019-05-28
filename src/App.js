2fcd221e8c0423b69f065464a776bbd563954c01
nmamidon <nmamidon@gmail.com>
2019-05-28 08:55:27 -0400

    let's see if i can make this process go faster

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I'm great thanks
+          That's fantastic to know
         </p>
         <a
           className="App-link"

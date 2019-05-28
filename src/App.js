885c58a9f9def9df04a1fc9bcf7a2649d85d97ae
nmamidon <nmamidon@gmail.com>
2019-05-28 08:51:25 -0400

    changed p one more time

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          how are you today
+          I'm great thanks
         </p>
         <a
           className="App-link"
